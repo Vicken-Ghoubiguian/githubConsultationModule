612f41aa03120085a988daaf3a18bceffa9f5b3b
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-11-19 19:39:03 +0100

    Initial commit for alghoritm

---
+++ psInit.ps1
@@ -0,0 +1,31 @@
+﻿<#
+
+#>
+
+$githubGetUserURL = "https://api.github.com/users/Vicken-Ghoubiguian"
+
+$githubUserRequest = Invoke-WebRequest -Uri $githubGetUserURL -Method Get
+
+$githubUserRequestsContent = $githubUserRequest.Content
+
+$githubUserRequestsJSONContent = @"
+               
+$githubUserRequestsContent
+"@
+
+$githubUserRequestsResult = ConvertFrom-Json -InputObject $githubUserRequestsJSONContent
+
+Write-Host -NoNewLine "------------------------------------------------------"
+
+$githubGetReposURL = "https://api.github.com/users/Vicken-Ghoubiguian/repos"
+
+$githubReposRequest = Invoke-WebRequest -Uri $githubGetReposURL -Method Get
+
+$githubReposRequestsContent = $githubReposRequest.Content
+
+$githubReposRequestsJSONContent = @"
+               
+$githubReposRequestsContent
+"@
+
+$githubReposRequestsResult = ConvertFrom-Json -InputObject $githubReposRequestsJSONContent
