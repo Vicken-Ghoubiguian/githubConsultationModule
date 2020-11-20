610ccb25a0f09492a623a58ed8a00ea6f49c8893
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-11-20 16:59:05 +0100

    Defining the main PSM1 file of the module

---
+++ gitHubConsultAPI.psm1
@@ -0,0 +1,43 @@
+﻿# Definition of the User Powershell class to define a user from the GitHub API...
+class User
+{
+
+    #
+    hidden [string]$login
+    hidden [string]$id
+    hidden [string]$avatarURL
+    hidden [string]$htmlURL
+    hidden [string]$type
+    hidden [string]$name
+    hidden [string]$blogURL
+    hidden [string]$location
+    hidden [bool]$isHireable
+    hidden [int]$publicReposCount
+
+
+    # User class constructor...
+    User([string]$login)
+    {
+        #
+        $githubGetUserURL = "https://api.github.com/users/Vicken-Ghoubiguian"
+        $githubUserRequest = Invoke-WebRequest -Uri $githubGetUserURL -Method Get
+        $githubUserRequestsContent = $githubUserRequest.Content
+        $githubUserRequestsJSONContent = @"
+                       
+$githubUserRequestsContent
+"@
+        $githubUserRequestsResult = ConvertFrom-Json -InputObject $githubUserRequestsJSONContent
+
+        #
+        $this.login =$githubUserRequestsResult.login
+        $this.id = $githubUserRequestsResult.id
+        $this.avatarURL = $githubUserRequestsResult.avatar_url
+        $this.htmlURL = $githubUserRequestsResult.html_url
+        $this.type = $githubUserRequestsResult.type
+        $this.name = $githubUserRequestsResult.name
+        $this.blogURL = $githubUserRequestsResult.blog
+        $this.location = $githubUserRequestsResult.location
+        $this.isHireable = $githubUserRequestsResult.hireable
+        $this.publicReposCount = $githubUserRequestsResult.public_repos
+    }
+}
