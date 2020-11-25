a0f27e63a96b9c1aca40d2d2d0af74b1f093965c
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-11-25 19:45:32 +0100

    Definition of the 'getUserFromLogin' function with all comments and some things done too

--- githubConsultationAPI.psm1
+++ githubConsultationAPI.psm1
@@ -6,13 +6,16 @@
 Using module .\usedModules\repository.psm1
 Using module .\usedModules\user.psm1
 
-#
+# Definition of the function 'getUserFromLogin' which returns the gitHub user identified by his login...
 function getUserFromLogin {
 
-    #
+    # Definition of the only parameter of this function, the user's login ...
     param (
         [string]$wishedLogin
     )
+
+    # Return the instance of the newly created User class from the login... 
+    return [User]::new($wishedLogin)
 }
 
 #
