614d43ef2b742a535580509dbea97d5c2da4f9b4
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-11-30 16:18:36 +0100

    Definition of the Powershell Branch class

---
+++ usedModules/branch.psm1
@@ -0,0 +1,42 @@
+﻿# Definition of the Branch Powershell class to define a license from the GitHub API...
+class Branch {
+
+    # All attributes of the Branch class...
+    hidden [string]$name
+    hidden [string]$lastCommitSha
+    hidden [string]$lastCommitURL
+    hidden [bool]$isProtected
+
+    # Branch class constructor...
+    Branch([string]$collectedName, [string]$collectedLastCommitSha, [string]$collectedLastCommitURL, [bool]$collectedIsProtected)
+    {
+        $this.name = $collectedName
+        $this.lastCommitSha = $collectedLastCommitSha
+        $this.lastCommitURL = $collectedLastCommitURL
+        $this.isProtected = $collectedIsProtected
+    }
+
+    # 'name' attribute getter...
+    [string] getName()
+    {
+        return $this.name
+    }
+
+    # 'lastCommitSha' attribute getter...
+    [string] getLasCommitSha()
+    {
+        return $this.lastCommitSha
+    }
+
+    # 'lastCommitURL' attribute getter...
+    [string] getLastCommitURL()
+    {
+        return $this.lastCommitURL
+    }
+
+    # 'isProtected' attribute getter...
+    [bool] getIsProtected()
+    {
+        return $this.isProtected
+    }
+}
