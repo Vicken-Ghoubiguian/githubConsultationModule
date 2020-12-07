6e51c4cc7169de6fc062f5915ce2095501cb83dc
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-07 08:42:30 +0100

    Process and recuperation of all datas in 'listAllBranches' static function in the 'Branch' Powershell class

--- usedModules/branch.psm1
+++ usedModules/branch.psm1
@@ -19,10 +19,14 @@ class Branch {
     # Definition of a static function to put all branches from a user and a repository identified respectively by its login and its name inside an array...
     Static [System.Array] listAllBranches([string]$wishedLogin, [string]$wishedRepos)
     {
-        #
+        # Create an HTTP request to take all branches informations from the GitHub repository identified by its name and its owner's login...
         $githubGetBranchesURL = "https://api.github.com/repos/" + $wishedLogin + "/" + $wishedRepos + "/branches"
 
-        return @("Ok", "Testy", "Testou")
+        #
+        $githubBranchesRequest = Invoke-WebRequest -Uri $githubGetBranchesURL -Method Get
+        $branchesJSONObj = ConvertFrom-Json $githubBranchesRequest.Content
+
+        return @($branchesJSONObj, "Testy", "Testou")
     }
 
     # 'name' attribute getter...
