4c1168b114c8c9e90bea95c6a756d36d2e43752f
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-07 09:59:25 +0100

    Development of the 'listAllBranches' static functions in the Powershell class 'Branch'

--- usedModules/branch.psm1
+++ usedModules/branch.psm1
@@ -19,14 +19,24 @@ class Branch {
     # Definition of a static function to put all branches from a user and a repository identified respectively by its login and its name inside an array...
     Static [System.Array] listAllBranches([string]$wishedLogin, [string]$wishedRepos)
     {
+        # Definition of the 'branchesArray' array which will contain all branches of the wished 'wishedRepos' repo from the wished 'wishedLogin' user...
+        $branchesArray = [System.Collections.ArrayList]::new()
+
         # Create an HTTP request to take all branches informations from the GitHub repository identified by its name and its owner's login...
         $githubGetBranchesURL = "https://api.github.com/repos/" + $wishedLogin + "/" + $wishedRepos + "/branches"
 
-        #
+        # Retrieving and extracting all branches received from the URL...
         $githubBranchesRequest = Invoke-WebRequest -Uri $githubGetBranchesURL -Method Get
         $branchesJSONObj = ConvertFrom-Json $githubBranchesRequest.Content
 
-        return @($branchesJSONObj, "Testy", "Testou")
+        # Browse all the branches contained in the received JSON and create all the instances of the Powershell class 'Branch' from this data and add them to the array 'branchesArray'...
+        foreach($branch in $branchesJSONObj) {
+
+            $branchesArray.Add([Branch]::new($branch.name, $branch.commit.sha, $branch.commit.url, $branch.protected))
+        }
+
+        # Returning the '$branchesArray' array...
+        return $branchesArray
     }
 
     # 'name' attribute getter...
