5fe82e1754fe7b5cc0909679a37d139c6cb2d93a
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-09 10:12:22 +0100

    First stêp for the definition of the 'Language' Powershell class

---
+++ language.psm1
@@ -0,0 +1,32 @@
+﻿#
+class Language {
+
+    #
+    hidden [string]$name
+    hidden [int]$value
+    hidden [int]$totalValue
+
+    # 'name' attribute getter...
+    [string] getName()
+    {
+        return $this.name
+    }
+
+    # 'value' attribute getter...
+    [int] getValue()
+    {
+        return $this.value
+    }
+
+    # 'percentage' calculator getter...
+    [float] getPercentage()
+    {
+        return ($this.value * 100)/$this.totalValue
+    }
+
+    # 'totalValue' attribute getter...
+    [int] getTotalValue()
+    {
+        return $this.totalValue
+    }
+}
