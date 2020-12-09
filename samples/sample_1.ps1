e10e1185ca5109ee17efc4795e34374a8977b228
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-09 08:54:28 +0100

    Finalisation of the official first sample of this Powershell module

--- samples/sample_1.ps1
+++ samples/sample_1.ps1
@@ -1,9 +1,14 @@
-﻿Using module ..\Repository.psm1
+﻿# Importation of the 'Repository' module...
+Using module ..\Repository.psm1
 
-$currentRepository = [Repository]::new("Vicken-Ghoubiguian","opencv")
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the wished repository's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
+
+# Creation of an instance of the Repository Powershell class with all wished parameters...
+$currentRepository = New-Object -TypeName Repository -ArgumentList $ownerLogin, $repositoryName
+
+# Display all collected informations about the wished repository in the Powershell console...
 $currentRepository.ToString()
-
-Write-Host "`n" "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" "`n"
-
-$anotherCurrentRepository = [Repository]::new("opencv", "Vicken-Ghoubiguian")
-$anotherCurrentRepository.ToString()
