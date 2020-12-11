2d40bf407226d1dba4a2227e0766ab8eb9b6e435
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-11 16:05:37 +0100

    Final configuration of the languageDisplayer PS script

--- samples/languagesDisplayer.ps1
+++ samples/languagesDisplayer.ps1
@@ -1,5 +1,18 @@
 ﻿# Importation of the 'Language' module...
 Using module ..\language.psm1
 
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the wished repository's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
+
 #
-$languagesArray = [Language]::listAllLanguages("Vicken-Ghoubiguian", "opencv")
+$languagesArray = [Language]::listAllLanguages($ownerLogin, $repositoryName)
+
+#
+foreach($language in $languagesArray) {
+
+    #
+    $language.ToString()
+}
