f541ed0dc4221b5f0eb80a5705fbb5b09f0984e1
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-13 19:04:07 +0100

    Complete now

--- samples/allRepositoryDisplayer.ps1
+++ samples/allRepositoryDisplayer.ps1
@@ -1,5 +1,16 @@
 ﻿# Importation of the 'Repository' module...
 Using module ..\repository.psm1
 
-#
-$repositoriesArray = [Repository]::listAllRepositories("Vicken-Ghoubiguian")
+# Definition of the only one parameter : '$ownerLogin' for the wished owner's name...
+param (
+    [string]$ownerLogin
+)
+
+# Get all repositories from the whished user identified by its login and put them in the "repositoriesArray" array...
+$repositoriesArray = [Repository]::listAllRepositories($ownerLogin.ToString())
+
+# Browse the array implemented previously and display each repos...
+foreach($repos in $repositoriesArray) {
+
+    $repos.ToString()
+}
