b89221589866f39738d4b357fc33b7ce75c2461a
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-14 11:52:55 +0100

    '' attribute and 'try...catch...' in 'organization' Powershell class

--- organization.psm1
+++ organization.psm1
@@ -3,10 +3,27 @@
 # Definition of the User Powershell class to define a user from the GitHub API...
 class Organization {
 
+    hidden [GitHubError]$error
+
     # Repository class constructor with organization login...
     Organization([string]$organizationLogin)
     {
         # Create an HTTP request to take the GitHub organization identified by its name and its owner's login...
         $githubGetReposURL = "https://api.github.com/orgs/" + $organizationLogin
+
+        # Bloc we wish execute to get all informations about the wished organization...
+        try {
+
+        # Bloc to execute if an System.Net.WebException is encountered...
+        } catch [System.Net.WebException] {
+
+            $errorType = $_.Exception.GetType().Name
+
+            $errorMessage = $_.Exception.Message
+
+            $errorStackTrace = $_.Exception.StackTrace
+
+            $this.error = [GitHubError]::new($errorType, $errorMessage, $errorStackTrace)
+        }
     }
 }
