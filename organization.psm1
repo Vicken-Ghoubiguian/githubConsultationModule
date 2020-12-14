37161113f65caab3c851f3c3b34e637c7929707f
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-14 11:26:27 +0100

    Defining the 'organization' Powershell class squeleton

---
+++ organization.psm1
@@ -0,0 +1,12 @@
+﻿Using module .\usefulClassesAndObjects\gitHubError.psm1
+
+# Definition of the User Powershell class to define a user from the GitHub API...
+class Organization {
+
+    # Repository class constructor with organization login...
+    Organization([string]$organizationLogin)
+    {
+        # Create an HTTP request to take the GitHub organization identified by its name and its owner's login...
+        $githubGetReposURL = "https://api.github.com/orgs/" + $organizationLogin
+    }
+}
