3ab4815270ed6a4a15ee8e72e531b23d2c6306d9
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-18 15:09:03 +0100

    Some addings for the repos displayer sample

--- samples/allRepositoryDisplayer.ps1
+++ samples/allRepositoryDisplayer.ps1
@@ -3,11 +3,13 @@ Using module ..\repository.psm1
 
 # Definition of the only one parameter : '$ownerLogin' for the wished owner's name...
 param (
-    [string]$ownerLogin
+    [string]$ownerLogin,
+    [bool]$branchesExpected,
+    [bool]$languagesExpected
 )
 
-# Get all repositories from the whished user identified by its login and put them in the "repositoriesArray" array...
-$repositoriesArray = [Repository]::listAllRepositories($ownerLogin.ToString(), $true, $true)
+# Get all repositories from the whished user identified by its login, with all branches and languages if expected, and put them in the "repositoriesArray" array...
+$repositoriesArray = [Repository]::listAllRepositories($ownerLogin.ToString(), $branchesExpected.ToBoolean(), $languagesExpected.ToBoolean())
 
 # Browse the array implemented previously and display each repos...
 foreach($repos in $repositoriesArray) {
