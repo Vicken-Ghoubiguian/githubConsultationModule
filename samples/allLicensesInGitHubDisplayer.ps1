7aa313e6275d53715eb825f364bad7ba33e7902c
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-19 19:30:31 +0100

    Implementation of a sample for all Licenses

---
+++ samples/allLicensesInGitHubDisplayer.ps1
@@ -0,0 +1,11 @@
+﻿# Importation of the 'License' module...
+Using module ..\usefulClassesAndObjects\license.psm1
+
+# Get all licenses available in GitHub and put them in the "licensesArray" array...
+$licensesArray = [License]::listAllDefinedLicenses()
+
+# Browse the array implemented previously and display each branch...
+foreach($license in $licensesArray) {
+
+    $license.ToString()
+}
