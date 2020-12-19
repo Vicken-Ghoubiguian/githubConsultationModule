59371c8eab3aa1a4b4c22a162d52577d9acc0d40
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-19 14:35:28 +0100

    Another sample for commits and modify comment

---
+++ samples/allCommitsDisplayer.ps1
@@ -0,0 +1,17 @@
+﻿# Importation of the 'Commit' module...
+Using module ..\commit.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the repository name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
+
+# Get all commits from the whished repository owned by the user identified by its login and put them in the "commitsArray" array...
+$commitsArray = [Commit]::listAllCommits($ownerLogin.ToString(), $repositoryName.ToString())
+
+# Browse the array implemented previously and display each commit...
+foreach($commit in $commitsArray) {
+
+    $commit.ToString()
+}
