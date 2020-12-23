1b608f30ff36dda568371c9f0624dd6666e2841d
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-23 16:11:28 +0100

    More things...

--- samples/allLicensesInGitHubDisplayer.ps1
+++ samples/allLicensesInGitHubDisplayer.ps1
@@ -4,8 +4,17 @@ Using module ..\usefulClassesAndObjects\license.psm1
 # Get all licenses available in GitHub and put them in the "licensesArray" array...
 $licensesArray = [License]::listAllDefinedLicenses()
 
+# Implementation of a message box that asks a very specific question...
+$additionalValuesLicenseBoxResponse = [System.Windows.MessageBox]::Show(". Do you want them ?", "Confirmation", "YesNo", "info")
+
 # Browse the array implemented previously and display each branch...
 foreach($license in $licensesArray) {
+
+    # If the response from previous message box is "Yes"...
+    If($additionalValuesLicenseBoxResponse -eq "Yes") {
+
+        $license.completeAllDatas()
+    }
 
     $license.ToString()
 }
