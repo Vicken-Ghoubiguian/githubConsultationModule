144b6e8eddf4852da41f430fdac399f5aed3e7e1
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-26 15:18:23 +0100

    New added sample for commits

---
+++ samples/commitDisplayer.ps1
@@ -0,0 +1,12 @@
+﻿# Importation of the 'Commit' module...
+Using module ..\Commit.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name, '$repositoryName' for the wished repository's name and '$commitSha' for the wished commit's sha...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName,
+    [string]$commitSha
+)
+
+# Creation of an instance of the Commit Powershell class with all wished parameters...
+$currentCommit = New-Object -TypeName Commit -ArgumentList $ownerLogin, $repositoryName, $commitSha
