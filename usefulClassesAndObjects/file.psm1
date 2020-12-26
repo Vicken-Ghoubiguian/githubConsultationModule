a362346e92638d212fa5185658a14bd5e0576dc9
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-26 19:05:00 +0100

    Definition of the newest 'File' Powershell class

---
+++ usefulClassesAndObjects/file.psm1
@@ -0,0 +1,30 @@
+﻿# Definition of the File Powershell class to define a file in a repository from the GitHub API...
+class File
+{
+    # All attributes of the File class...
+    hidden [string]$sha
+    hidden [string]$filename
+    hidden [string]$status
+
+    hidden [int]$additions
+    hidden [int]$deletions
+    hidden [int]$changes
+
+    hidden [string]$blob
+    hidden [string]$raws
+    hidden [string]$contents
+    hidden [string]$patch
+
+    # File class constructor with user login, repository name and file's name... 
+    File([string]$wishedUserLogin, [string]$wishedRepositoryName, [string]$wishedFileName)
+    {
+        # Extract all the data relating to the desired file identified by the desired user login, the desired repository name and desired file's name...
+        $githubGetFileURL = "https://api.github.com/repos/" + $wishedUserLogin + "/" + $wishedRepositoryName + "/contents/" + $wishedFileName
+    }
+
+    # File class constructor with all parameters...
+    File()
+    {
+
+    }
+}
