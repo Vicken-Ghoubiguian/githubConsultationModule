ffa5ac55032f2978bce31dcdf5eb92af4ea7df98
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-28 10:53:37 +0100

    Finalisation of the definition of the 'Get-All-GitHub-Icons' function in the module previously defined

--- usefulClassesAndObjects/emojis.psm1
+++ usefulClassesAndObjects/emojis.psm1
@@ -6,12 +6,25 @@ function Get-All-GitHub-Icons {
     #
     $allEmojis = $null
 
-    # Create an HTTP request to take all GitHub icons...
-    $githubGetAllGitHubIconsURL = "https://api.github.com/emojis"
+    # Create an HTTP request to take all GitHub emojis...
+    $githubGetAllGitHubEmojisURL = "https://api.github.com/emojis"
 
     # Bloc we wish execute to get all informations about GitHub emojis...
     try {
         
+        # Retrieving and extracting all emojis received from the URL...
+        $githubAllGitHubEmojisRequest = Invoke-WebRequest -Uri $githubGetAllGitHubEmojisURL -Method Get
+        $allGitHubEmojisJSONObj = ConvertFrom-Json $githubAllGitHubEmojisRequest.Content
+
+        #
+        $allEmojis = @()
+
+        #
+        foreach($emoji in $allGitHubEmojisJSONObj.PSObject.Properties) {
+
+            $allEmojis[$emoji.Name] = $emoji.Value
+        }
+
     # Bloc to execute if an System.Net.WebException is encountered...
     } catch [System.Net.WebException] {
 
