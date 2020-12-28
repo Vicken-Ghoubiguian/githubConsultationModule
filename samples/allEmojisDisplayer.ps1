60e299c66a64f95f96ac094c82bb01765bf057d1
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-28 11:38:18 +0100

    Final touch for the previous sample

--- samples/allEmojisDisplayer.ps1
+++ samples/allEmojisDisplayer.ps1
@@ -2,3 +2,9 @@
 
 # Call of the famous function 'Get_All_GitHub_Icons'...
 $allEmojis = Get_All_GitHub_Icons
+
+#
+foreach($emoji in $allEmojis.GetEnumerator()) {
+
+    Write-Host "$($emoji.Name): $($emoji.Value)"
+}
