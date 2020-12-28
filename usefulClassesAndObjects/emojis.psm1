38c39dbf572e14e6a202c36f87e95b6cd85d0665
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-28 10:25:40 +0100

    Definition of a function to take and return all GitHub emojis

---
+++ usefulClassesAndObjects/emojis.psm1
@@ -0,0 +1,10 @@
+﻿Using module .\usefulClassesAndObjects\gitHubError.psm1
+
+#
+function Get-All-GitHub-Icons {
+
+    # Create an HTTP request to take all GitHub icons...
+    $githubGetAllGitHubIconsURL = "https://api.github.com/emojis"
+
+
+}
