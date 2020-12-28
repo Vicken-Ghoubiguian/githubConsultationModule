2953ff8920fb7003bccfa1d0f6c5c136988b719a
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-28 11:42:32 +0100

    Writing some comments

--- samples/allEmojisDisplayer.ps1
+++ samples/allEmojisDisplayer.ps1
@@ -3,10 +3,10 @@
 # Call of the famous function 'Get_All_GitHub_Icons'...
 $allEmojis = Get_All_GitHub_Icons
 
-#
+# Here a line break...
 Write-Host "`n"
 
-#
+# Browse the associative array containing all the emojis and displaing them...
 foreach($emoji in $allEmojis.GetEnumerator()) {
 
     Write-Host "$($emoji.Name): $($emoji.Value)" + "`n"
