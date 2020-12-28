232751b0b8cd6e27f010f147c932a11647be799a
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-28 10:35:49 +0100

    Completage of previous tommorow

--- usefulClassesAndObjects/emojis.psm1
+++ usefulClassesAndObjects/emojis.psm1
@@ -3,8 +3,27 @@
 #
 function Get-All-GitHub-Icons {
 
+    #
+    $allEmojis = $null
+
     # Create an HTTP request to take all GitHub icons...
     $githubGetAllGitHubIconsURL = "https://api.github.com/emojis"
 
+    # Bloc we wish execute to get all informations about GitHub emojis...
+    try {
+        
+    # Bloc to execute if an System.Net.WebException is encountered...
+    } catch [System.Net.WebException] {
 
+        $errorType = $_.Exception.GetType().Name
+
+        $errorMessage = $_.Exception.Message
+
+        $errorStackTrace = $_.Exception.StackTrace
+
+        $allEmojis = [GitHubError]::new($errorType, $errorMessage, $errorStackTrace)
+    }
+
+    # returning the '$allEmojis' variable...
+    return $allEmojis
 }
