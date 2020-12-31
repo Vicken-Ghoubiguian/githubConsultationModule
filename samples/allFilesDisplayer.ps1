33cfe190fae84cf23236ff9611de9733fbd5887d
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2020-12-31 13:22:37 +0100

    Definition of the 'allFilesDisplayer' PowerShell sample

---
+++ samples/allFilesDisplayer.ps1
@@ -0,0 +1,11 @@
+﻿# Importation of the 'File' module...
+Using module ..\usefulClassesAndObjects\file.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the wished repository's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
+
+# Get all files from the whished repository owned by the user identified by its login and put them in the "filesArray" array...
+$filesArray = [File]::listAllFilesInRepos($ownerLogin.ToString(), $repositoryName.ToString())
