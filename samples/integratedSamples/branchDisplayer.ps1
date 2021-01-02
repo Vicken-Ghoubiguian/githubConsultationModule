b3364d767e0f231cbb341d29107507abd3d1bdcf
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-02 18:20:16 +0100

    Development of the 'branchDisplayer' PowerShell script

---
+++ samples/integratedSamples/branchDisplayer.ps1
@@ -0,0 +1,15 @@
+﻿# Importation of the 'Branch' module...
+Using module ..\..\Branch.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name, '$repositoryName' for the wished repository's name and '$branchName' for the wished branch's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName,
+    [string]$branchName
+)
+
+# Creation of an instance of the Branch Powershell class with the wished parameter...
+$currentBranch = New-Object -TypeName Branch -ArgumentList $ownerLogin, $repositoryName, $branchName
+
+# Display all collected informations about the wished branch in the Powershell console...
+$currentBranch.ToString()
