42d71b6bbc4b440e6d725042eb9b61c66d58f392
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-05 16:28:02 +0100

    Implementation of the 'repositoriesCount' PowerShell sample

---
+++ samples/integratedSamples/repositoriesCount.ps1
@@ -0,0 +1,13 @@
+﻿# Importation of the 'usefulFunctions' module...
+Using module ..\..\usefulFunctions.psm1
+
+# Definition of the only required parameter: '$ownerLogin' for the wished owner's name...
+param (
+    [string]$ownerLogin
+)
+
+#
+$reposCount = Get_Total_Number_Of_Repos -ownerLogin $ownerLogin.ToString()
+
+# Displaying repos count...
+Write-Host $reposCount
