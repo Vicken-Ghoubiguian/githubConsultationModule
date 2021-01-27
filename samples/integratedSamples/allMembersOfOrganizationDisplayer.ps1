a1db72574b3b05cf3f9c9641451cdf58b9288635
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-27 15:54:22 +0100

    New sample for organizations' members

---
+++ samples/integratedSamples/allMembersOfOrganizationDisplayer.ps1
@@ -0,0 +1,18 @@
+﻿# Importation of the 'Organization' module...
+Using module ..\..\Organization.psm1
+
+# Definition of the only parameter '$organizationLogin' for the wished organization's name...
+param (
+    [string]$organizationLogin
+)
+
+# Get all members of the whished organization identified by its login...
+$membersArray = [Organization]::listAllMembers($organizationLogin.ToString())
+
+# Browse the array implemented previously and display each member...
+foreach($member in $membersArray) {
+
+    $member
+
+    Write-Host "`n"
+}
