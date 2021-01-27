c7fba9e393ec717004deb2cd01bde9f83fb32362
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-01-27 15:16:22 +0100

    2 new samples about the two other static functions of 'orgs' and 'user'

---
+++ samples/integratedSamples/allRepositoryDisplayerFromOrganization.ps1
@@ -0,0 +1,18 @@
+﻿# Importation of the 'Organization' module...
+Using module ..\..\Organization.psm1
+
+# Definition of all parameters : '$organizationLogin' for the wished organization's name, '$branchesExpected' and '$languagesExpected' that are 2 boolean indicators to specifie if we want all branches and languages respectively or not...
+param (
+    [string]$organizationLogin,
+    [bool]$branchesExpected,
+    [bool]$languagesExpected
+)
+
+# Get all repositories from the whished user identified by its login, with all branches and languages if expected, and put them in the "repositoriesArray" array...
+$repositoriesArray = [Organization]::listAllReposFromOrganization($organizationLogin.ToString(), $withBranches, $withLanguages)
+
+# Browse the array implemented previously and display each repos...
+foreach($repos in $repositoriesArray) {
+
+    $repos.ToString()
+}
---
+++ samples/integratedSamples/allRepositoryDisplayerFromUser.ps1
@@ -0,0 +1,18 @@
+﻿# Importation of the 'User' module...
+Using module ..\..\User.psm1
+
+# Definition of all parameters : '$userLogin' for the wished user's name, '$branchesExpected' and '$languagesExpected' that are 2 boolean indicators to specifie if we want all branches and languages respectively or not...
+param (
+    [string]$userLogin,
+    [bool]$branchesExpected,
+    [bool]$languagesExpected
+)
+
+# Get all repositories from the whished user identified by its login, with all branches and languages if expected, and put them in the "repositoriesArray" array...
+$repositoriesArray = [User]::listAllReposFromUser($userLogin.ToString(), $withBranches, $withLanguages)
+
+# Browse the array implemented previously and display each repos...
+foreach($repos in $repositoriesArray) {
+
+    $repos.ToString()
+}
