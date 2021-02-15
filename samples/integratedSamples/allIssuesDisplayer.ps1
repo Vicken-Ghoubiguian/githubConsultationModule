c92050c776e711f92e31a197f89c77591425dd21
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-15 18:31:04 +0100

    All displaying issues example and some proivisional things

---
+++ samples/integratedSamples/allIssuesDisplayer.ps1
@@ -0,0 +1,11 @@
+﻿# Importation of the 'Issue' module...
+Using module ..\..\Issue.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the wished repository's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
+
+# Get all issues from the whished repository owned by the user identified by its login and put them in the "issuesArray" array...
+$issuesArray = [Issue]::listAllIssues("Screenly", "screenly-ose")
