a984e615d0f0c019ec3bc1eae7be2c5698ed6bc4
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-16 18:28:19 +0100

    Introduction of the sample simple for the 'Issue' PowerShell class

---
+++ samples/integratedSamples/issueDisplayer.ps1
@@ -0,0 +1,14 @@
+﻿# Importation of the 'Issue' module...
+Using module ..\..\Issue.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the wished repository's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
+
+# Creation of an instance of the Issue Powershell class with all wished parameters...
+$currentIssue = New-Object -TypeName Issue -ArgumentList $ownerLogin, $repositoryName
+
+# Display all collected informations about the wished issue in the Powershell console...
+$currentIssue.ToString()
