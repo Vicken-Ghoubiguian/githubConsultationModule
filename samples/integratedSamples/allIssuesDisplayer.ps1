1da94af5d9ba6d085203e91b708f4356482d699c
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-16 17:37:57 +0100

    'ToString' in display sample

--- samples/integratedSamples/allIssuesDisplayer.ps1
+++ samples/integratedSamples/allIssuesDisplayer.ps1
@@ -9,3 +9,9 @@ param (
 
 # Get all issues from the whished repository owned by the user identified by its login and put them in the "issuesArray" array...
 $issuesArray = [Issue]::listAllIssues($ownerLogin.ToString(), $repositoryName.ToString())
+
+# Browse the array implemented previously and display each issues...
+foreach($issue in $issuesArray) {
+
+    $issue.ToString()
+}
