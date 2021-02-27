291964fa96b85c8b188f37cded4be187c84c9648
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-27 10:29:41 +0100

    Other things added

--- comment.psm1
+++ comment.psm1
@@ -4,5 +4,30 @@ Using module .\gitHubError.psm1
 # Definition of the Comment Powershell class to define a issue from the GitHub API...
 class Comment
 {
+    # All attributes of the Comment class...
+    hidden [int]$id
+    hidden [string]$nodeId
+    hidden [string]$htmlUrl
+    hidden [string]$issueUrl
+    hidden [string]$Url
 
+    hidden [int]$userId
+    hidden [string]$userLogin
+    hidden [string]$userNodeId
+    hidden [string]$userAvatar
+    hidden [string]$userUrl
+    hidden [string]$userHtmlUrl
+    hidden [bool]$userSiteAdmin
+    hidden [string]$userType
+
+    hidden [dateTime]$creatingDate
+    hidden [dateTime]$updatingDate
+    hidden [string]$authorAssociation
+    hidden [string]$body
+
+    # Comment class constructor...
+    Comment()
+    {
+        $githubGetCommentURL = "https://api.github.com/repos/Screenly/Screenly-OSE/issues/comments/7364581"
+    }
 }
