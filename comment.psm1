22016fe56054c245a90ce0e19fa695f2d8492824
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-02-27 10:07:00 +0100

    forgot it :-((

---
+++ comment.psm1
@@ -0,0 +1,8 @@
+﻿# Importation of the 'GitHubError' module...
+Using module .\gitHubError.psm1
+
+# Definition of the Comment Powershell class to define a issue from the GitHub API...
+class Comment
+{
+
+}
