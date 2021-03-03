e15cf7fdb5f608be125eb65fa57cde4727b1986f
Eric Ghoubiguian <eric.ghoubiguian@imerir.com>
2021-03-03 12:49:28 +0100

    Definition of parameters in coming samples

--- samples/integratedSamples/allCommentsFromIssueDisplayer.ps1
+++ samples/integratedSamples/allCommentsFromIssueDisplayer.ps1
@@ -1,2 +1,9 @@
 ﻿# Importation of the 'Comment' module...
 Using module ..\..\comment.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name, '$repositoryName' for the wished repository's name and '$issueNumber' for the wished issue number...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName,
+    [int]$issueNumber
+)
--- samples/integratedSamples/allCommentsFromReposDisplayer.ps1
+++ samples/integratedSamples/allCommentsFromReposDisplayer.ps1
@@ -1,2 +1,8 @@
 ﻿# Importation of the 'Comment' module...
 Using module ..\..\comment.psm1
+
+# Definition of all parameters : '$ownerLogin' for the wished owner's name and '$repositoryName' for the wished repository's name...
+param (
+    [string]$ownerLogin,
+    [string]$repositoryName
+)
